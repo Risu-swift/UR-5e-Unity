b4bef32f6f26c811e8834dc463c1d3d8fb457691
saranr7 <saran.1902210@srec.ac.in>
2021-03-26 20:44:13 +0530

    Added Destroy and transform while throwing cubes

---
+++ Assets/destroyonthrow.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyonthrow : MonoBehaviour
+{
+    public GameObject here;
+    bool exit = false;
+    private GameObject currentobject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // here = GameObject.Find("respawn_point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (exit)
+        {
+            currentobject.transform.position = here.transform.position;
+            //
+        }
+        
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Collider>().gameObject.layer == 11)
+        {
+            currentobject = other.GetComponent<Collider>().gameObject;
+            exit = true;
+
+            // Destroy(other.GetComponent<Collider>().gameObject);
+
+            Debug.Log("Destroyed");
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().gameObject.layer == 11)
+        {
+            exit = false;
+            Debug.Log("Respawned");
+        }
+    }
+}
