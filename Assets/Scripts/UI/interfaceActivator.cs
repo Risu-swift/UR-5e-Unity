7d3d05e67bab6f9383474a9d92a4ff03bac6ddad
saranr7 <saran.1902210@srec.ac.in>
2021-04-25 11:03:21 +0530

    Integration of already finished TT court and Digital twin project
    Few changes have been made to the canvas for the UI interface for ray casting with buttons from controllers.

---
+++ Assets/Scripts/UI/interfaceActivator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interfaceActivator : MonoBehaviour
+{
+    public GameObject interfacepanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        interfacepanel = GameObject.Find("Interface");
+        interfacepanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="CamRig")
+        {
+            Debug.Log("ENTER");
+            interfacepanel.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "CamRig")
+        {
+            interfacepanel.SetActive(false);
+        }
+    }
+}
